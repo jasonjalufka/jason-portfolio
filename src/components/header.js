576ae0a3db4da8944515e5e188b71b071e982ed8
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-14 14:52:06 -0700

    Add navigation to Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,63 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+import theme from "../theme"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const NavList = styled.ul`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  margin-right: 5rem;
+`
+
+const NavItem = styled.li`
+  list-style-type: none;
+  padding: 1rem 1.5rem;
+`
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 24px;
+  font-weight: 300;
+  position: relative;
+  padding: 5px;
+  transition: color 200ms ease-in-out;
+
+  &:hover {
+    /* color: ${theme.colors.jumbo}; */
+    &:before {
+      height: 50%;
+      background-color: ${theme.colors.meatBrown};
+    }
+  }
+
+  &:before {
+    background-color: ${theme.colors.meatBrown};
+    content: "";
+    height: 0;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    transition: height 0.2s ease-in-out;
+    z-index: -1;
+  }
+`
+
+const Header = ({ menuLinks, siteTitle }) => (
+  <header>
+    <div>
+      <nav>
+        <NavList>
+          {menuLinks.map(link => (
+            <NavItem key={link.name}>
+              <NavLink to={link.link}>{link.name}</NavLink>
+            </NavItem>
+          ))}
+        </NavList>
+      </nav>
     </div>
   </header>
 )
