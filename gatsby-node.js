772bdd41fb841a22941958fc41fbfe734a922bfb
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-17 18:54:50 -0700

    Create gatsby-node, generate slugs, add aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,60 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, `src/components`),
+        "@config": path.resolve(__dirname, `src/config`),
+        "@content": path.resolve(__dirname, `src/content`),
+        "@images": path.resolve(__dirname, `src/images`),
+        "@pages": path.resolve(__dirname, `src/pages`),
+        "@templates": path.resolve(__dirname, `src/templates`),
+        "@styles": path.resolve(__dirname, `src/styles`),
+      },
+    },
+  })
+}
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    // console.log(node)
+    console.log(createFilePath({ node, getNode, basePath: `content` }))
+    const slug = createFilePath({ node, getNode, basePath: `content` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  // Note: the graphql function call returns a Promise
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/work-template.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
