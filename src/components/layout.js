02c6b23118525b930277948a47ed294fa770fc88
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-17 17:14:26 -0700

    Add SEO component to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import GlobalStyle from "../theme/globalStyle"
-
-import Header from "./header"
+import GlobalStyle from "@styles/globalStyle"
+import { Header } from "@components"
+import { ThemeProvider } from "styled-components"
+import theme from "@styles"
+import SEO from "./seo"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,23 +30,26 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <GlobalStyle />
-      <Header
-        menuLinks={data.site.siteMetadata.menuLinks}
-        siteTitle={data.site.siteMetadata.title}
-      />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <SEO />
+        <Header
+          menuLinks={data.site.siteMetadata.menuLinks}
+          siteTitle={data.site.siteMetadata.title}
+        />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+        </div>
+      </>
+    </ThemeProvider>
   )
 }
 
