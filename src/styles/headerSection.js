7dbcad6227ea3780291b7db4a174bccf617bb136
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-21 19:43:47 -0700

    Create new style for HeaderSection

---
+++ src/styles/headerSection.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import mixins from "./mixins"
+
+const HeaderSection = styled.section`
+  ${mixins.headerPadding};
+  max-width: none;
+  height: 50vh;
+`
+
+export default HeaderSection
--- src/styles/index.js
+++ src/styles/index.js
@@ -3,7 +3,17 @@ import GlobalStyle from "./globalStyle"
 import Main from "./main"
 import media from "./media"
 import mixins from "./mixins"
+import HeaderSection from "./headerSection"
 import Section from "./section"
 import Heading from "./heading"
 
-export { theme, GlobalStyle, Main, media, mixins, Section, Heading }
+export {
+  theme,
+  GlobalStyle,
+  Main,
+  media,
+  mixins,
+  HeaderSection,
+  Section,
+  Heading,
+}
