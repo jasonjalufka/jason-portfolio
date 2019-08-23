d4051bd4d71c00b6898d4a9ccfaba3f3701a47ca
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-22 23:25:27 -0700

    Remove unused code

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react"
-import { email } from "@config"
 import styled from "styled-components"
 import { animations, theme, mixins, media, Section } from "@styles"
 import { DataContext } from "@components/layout"
@@ -62,25 +61,11 @@ const WeatherContainer = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
-  /* ${media.giant`left: -120px;`};
-  ${media.bigDesktop`left: -120px;`};
-  ${media.desktop`
-    bottom: -35vh;
-    left: -70px;
-  `};
-  ${media.tablet`
-    left: -20px;
-    bottom: -33vh;
-  `};
-  ${media.phablet`display: none;`}; */
 `
 
 const Weather = styled.h3`
   font-size: 26px;
   font-weight: 200;
-  /* position: relative;
-  bottom: 0;
-  left: 0; */
 `
 
 const SocialContainer = styled.div`
